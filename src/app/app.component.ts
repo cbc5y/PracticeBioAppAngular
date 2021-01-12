4aa22e0208b89f6c24cecfabc38d85c29f9be78b
Christian <ccastaneda1200@gmail.com>
2021-01-12 00:18:55 -0600

    edited further

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
       styleUrls: ['./app.component.css']
     })
     export class AppComponent {
-      title = 'Margaret Hamilton';
-      city = 'Paoli, Indiana';
-      tagline = 'There was no choice but to be pioneers; no time to be beginners.';
-      aboutMe = 'I am an American computer scientist, systems engineer, and business owner. I was Director of the Software Engineering Division of the MIT Instrumentation Laboratory, which developed on-board flight software for the Apollo space program. In 1986, I became the founder and CEO of Hamilton Technologies, Inc., in Cambridge, Massachusetts. The company was developed around the Universal Systems Language based on my paradigm of Development Before the Fact (DBTF) for systems and software design. I have published over 130 papers, proceedings, and reports about the 60 projects and six major programs in which I have been involved. On November 22, 2016, I was awarded the Presidential Medal of Freedom by U.S. President Barack Obama for my work leading the development of on-board flight software for NASAs Apollo Moon missions.'
+      title = 'Christian Castaneda';
+      city = 'Nashville, Tennessee';
+      tagline = 'I want to be great more than I want to feel good';
+      aboutMe =  'I am Christian Castaneda. Check out my social media below! I love coding and am excited about my future in the industry. I especially enjoy working with Angular and NodeJS. I have the most amount of experience with the M.E.A.N. Stack. When I am not sharpening my skills in programming, I am an associate youth pastor at my church every Wednesdays. Some other hobbies I enjoy are hiking, fishing, reading, & studying theology.';
     }
